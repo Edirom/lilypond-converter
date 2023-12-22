81d4aad59cc8143475e8c5f4cc3c57241e792a03
Anne Ferger <anne.ferger@uni-paderborn.de>
2023-12-22 13:43:11 +0100

    Update required.sh

--- required.sh
+++ required.sh
@@ -4,7 +4,7 @@
 #https://gitlab.com/lilypond/lilypond/-/releases/permalink/latest
 #https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
 mkdir -p /tmp/lilypond
-#curl -L -o /tmp/lilypond.tar.gz https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
+curl -L -o /tmp/lilypond.tar.gz https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
 tar -xf /tmp/lilypond.tar.gz -C /tmp/lilypond
 mv /tmp/lilypond/*/* /opt/lilypond
 ln -s /opt/lilypond/bin/lilypond /usr/local/bin/lilypond
