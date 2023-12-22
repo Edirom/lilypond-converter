f1cb7d3307a3e250439ad0e60783a216fd2ab04d
Anne Ferger <anne.ferger@uni-paderborn.de>
2023-12-22 13:30:50 +0100

    Update required.sh

--- required.sh
+++ required.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # installs lilypond into ~/lilypond/usr
-curl -L -o /tmp/lilypond.tar.gz https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
+
 
 #https://gitlab.com/lilypond/lilypond/-/releases/permalink/latest
 #https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
-
-mkdir -p ~/lilypond/usr
+mkdir -p /opt/lilypond
 mkdir -p /tmp/lilypond
+curl -L -o /tmp/lilypond.tar.gz https://gitlab.com/lilypond/lilypond/-/releases/v2.25.11/downloads/lilypond-2.25.11-linux-x86_64.tar.gz
 tar -xf /tmp/lilypond.tar.gz -C /tmp/lilypond
-mv /tmp/lilypond/*/* ~/lilypond/usr
+mv /tmp/lilypond/*/* /opt/lilypond
+export PATH=$PATH:/opt/lilypond/bin
 lilypond --version
